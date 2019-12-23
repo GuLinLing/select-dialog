5b9a5c7769881a53a48401c9f6018b0dd946c23b
顾林岭 <gulinling1997@qq.com>
2019-12-23 17:00:02 +0800

    Create en.js

---
+++ lang/en.js
@@ -0,0 +1,8 @@
+export default {
+  placeholder: 'Searching for Space Members and Groups',
+  user: 'Contact',
+  group: 'Group',
+  tip: 'Sorry, no related data is found.',
+  confirm: 'Confirm',
+  cancel: 'Cancle'
+}
